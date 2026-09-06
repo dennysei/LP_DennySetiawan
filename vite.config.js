efed668479dbec3b736e0e54ef708b0642c5fc36
dennysei <dennysetiawan056@gmail.com>
2026-09-06 21:57:03 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/denny-portfolio/', // Tambahan untuk GitHub Pages
+  base: '/LP_DennySetiawan/', // Sesuai dengan nama repository lo
 })
